f48fff2f1797f0e2c8a1ce63135f7a1d9d5857d6
Liubomyr Halamaha <galamagal86@gmail.com>
2025-11-21 13:24:56 +0200

    upfate login

--- src/test/java/com/greencity/ui/TestWithLogin.java
+++ src/test/java/com/greencity/ui/TestWithLogin.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 public class TestWithLogin extends TestRunnerWithUser {
     @Test
     public void firstTest() {
-        WebElement image = homePage.getHeader().getLogo();
+        WebElement image = homePage.getHeader().getHeaderLogo();
         Assert.assertTrue(image.isDisplayed(), "The element is not displayed.");
     }
 }
