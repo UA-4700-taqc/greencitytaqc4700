d7d307ec3f0399061739b5dbaf42e76ae0e1eaea
alyonaMol <alen4hukm@gmail.com>
2025-11-25 18:41:14 +0200

    Implement Subscription section test case

---
+++ src/test/java/com/greencity/ui/SubscriptionSectionTest.java
@@ -0,0 +1,32 @@
+package com.greencity.ui;
+
+import com.greencity.ui.components.SubscriptionSectionComponent;
+import com.greencity.ui.pages.homepage.HomePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SubscriptionSectionTest extends BaseTest {
+
+    @Test(description = "Verify section visibility and static elements")
+    public void verifySubscriptionSectionVisibilityAndElements() {
+
+        HomePage homePage = new HomePage(driver);
+        SubscriptionSectionComponent subscriptionSection = new SubscriptionSectionComponent(driver);
+        subscriptionSection.scrollToSection();
+
+        Assert.assertTrue(subscriptionSection.isSectionDisplayed(), "The subscription section should be visible after scrolling.");
+
+        String expectedTittle = "Receive interesting news";
+        Assert.assertEquals(subscriptionSection.getSectionTitleText(),expectedTittle, "The text of the subscription section heading does not match what is expected.");
+
+        Assert.assertTrue(subscriptionSection.getQrCodeImage(), "The QR code for the mobile version should be displayed.");
+
+        String expectedInfoText = "Subscribe for our newsletter and always be up to date with news and updates";
+        Assert.assertEquals(subscriptionSection.getInfoText(), expectedInfoText, "The informational text of the section does not meet expectations.");
+
+        String expectedPlaceholder = "Enter your email";
+        Assert.assertEquals(subscriptionSection.getEmailInputPlaceholder(), expectedPlaceholder, "The email field placeholder does not match the expected one.");
+
+        Assert.assertTrue(subscriptionSection.getSubscribeButton().isDisplayed());
+    }
+}
