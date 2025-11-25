976bc4c298381ea92d2f40069abb52cfa5641c02
Liubomyr Halamaha <galamagal86@gmail.com>
2025-11-25 16:08:01 +0200

    update lhalam

--- src/main/java/com/greencity/ui/components/createnews/ContentSection.java
+++ src/main/java/com/greencity/ui/components/createnews/ContentSection.java
@@ -2,17 +2,11 @@ package com.greencity.ui.components.createnews;
 
 import com.greencity.ui.components.BaseComponent;
 import lombok.Getter;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class ContentSection extends BaseComponent {
 
@@ -26,9 +20,9 @@ public class ContentSection extends BaseComponent {
 
     @Getter
     @FindBy(xpath = ".//span[contains(@class,'field-info')]")
-    private  WebElement titleSymbolCount;
+    private WebElement titleSymbolCount;
 
-    @FindBy(xpath = ".//quill-editor")
+    @FindBy(xpath = ".//quill-editor//*[contains(@class, 'ql-editor')]")
     private WebElement contentInput;
 
     public ContentSection(WebDriver driver, WebElement rootElement) {
@@ -46,14 +40,9 @@ public class ContentSection extends BaseComponent {
     }
 
     public void enterContent(String text) {
-        new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.elementToBeClickable(contentInput));
-        ((JavascriptExecutor) driver).executeScript(
-                "arguments[0].innerHTML = arguments[1];", contentInput, text);
-        Actions actions = new Actions(driver);
-        actions.moveToElement(contentInput)
-                .click()
-                .sendKeys(Keys.ENTER)
-                .perform();
+        wait.until(ExpectedConditions.elementToBeClickable(contentInput));
+//        threadJs.executeScript("arguments[0].innerHTML = arguments[1];", contentInput, text);
+//        actions.moveToElement(contentInput).click().sendKeys(Keys.ENTER).perform();
+        contentInput.sendKeys(text);
     }
 }
--- src/main/java/com/greencity/ui/pages/homepage/HomePage.java
+++ src/main/java/com/greencity/ui/pages/homepage/HomePage.java
@@ -1,11 +1,7 @@
 package com.greencity.ui.pages.homepage;
 
-import com.greencity.ui.components.header.HeaderComponent;
 import com.greencity.ui.pages.BasePage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends BasePage {
 
