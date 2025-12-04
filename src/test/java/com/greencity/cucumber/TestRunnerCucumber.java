a903394e7d9a6eb1a0f038adbb3fb8b62c640449
Liubomyr Halamaha <galamagal86@gmail.com>
2025-12-04 13:15:50 +0200

    add before after in TestRunnerCucumber

--- src/test/java/com/greencity/cucumber/TestRunnerCucumber.java
+++ src/test/java/com/greencity/cucumber/TestRunnerCucumber.java
@@ -1,21 +1,23 @@
 package com.greencity.cucumber;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.testng.*;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
 
 @CucumberOptions(features = "src/test/resources/features", glue = "com.greencity.cucumber.steps")
 public class TestRunnerCucumber extends AbstractTestNGCucumberTests {
 
     private TestNGCucumberRunner testNGCucumberRunner;
+    protected SoftAssert softAssert;
 
     @BeforeClass
     public void setUpClass() {
 
         testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
     }
+
 
     @Test(description = "Example of BDD suite", dataProvider = "scenarios")
     public void scenario(PickleWrapper pickle, FeatureWrapper feature) {
@@ -33,5 +35,16 @@ public class TestRunnerCucumber extends AbstractTestNGCucumberTests {
     public void tearDownClass() {
         testNGCucumberRunner.finish();
     }
+
+    @Before
+    public void beforeScenario() {
+        softAssert = new SoftAssert();
+    }
+
+    @After
+    public void afterScenario() {
+        softAssert.assertAll();
+    }
+
 
 }
